0e62feb809b6b19ba49a5795da6aed15edb66294
kewining <kewining@kewining-linux>
2014-02-01 14:48:15 -0500

    Archivos de demos subidos...

---
+++ demo_nb/src/app/views/hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.views;
+
+/**
+ *
+ * @author kewining
+ */
+public class hello {
+    public static void main (String args [])
+    {
+        System.out.print("Hola mundo en java usando NB y Github.");
+    }
+}
