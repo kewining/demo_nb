87cfef4dd4460ae49652200c14c306dd1a3d58a8
kewining <kewining@kewining-linux>
2014-02-02 11:51:24 -0500

    Creacion de directorios model, controller y config este ultimo con dos archivos java para la conexion a base de datos

---
+++ demo_nb/src/app/config/connection_db.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author kewining
+ */
+public class connection_db {
+    public static Connection getConnection() {
+        
+        Connection cn = null;
+        try {
+            Class.forName(database.driver);
+            String url = database.server+database.data_base;
+            String user = database.user;
+            String password = database.password;
+            cn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error no se puede cargar el driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error no se establecer la conexion: " + e.getMessage());
+        }
+        
+//        if(cn!=null)
+//        {
+//            System.out.println("Conexion exitosa!");
+//        }
+//        else
+//        {
+//            System.out.println("Conexion fallida!");
+//        }
+        
+        return cn;
+    }
+//    public static void main(String args[])
+//    {
+//        getConnection();
+//    }
+}
---
+++ demo_nb/src/app/config/database.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.config;
+
+/**
+ *
+ * @author kewining
+ */
+public class database {
+    public static String driver = "com.mysql.jdbc.Driver";
+    public static String server = "jdbc:mysql://127.0.0.1:3306/";
+    public static String data_base = "demo";
+    public static String user = "root";
+    public static String password = "";
+}
